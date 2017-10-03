e2a3ded71f2e746b4646d10a011610e512207b7e
root <dexmount@hotmail.com>
2017-10-02 19:36:45 -0500

    added 2 more levels to the gauntlet

--- gauntlet.sh
+++ gauntlet.sh
@@ -12,5 +12,18 @@ mkdir .$directory\2
 mkdir $directory\3
 touch $directory\3/new
 
-#directory in random location
+#directory in /etc/
+mkdir /etc/$directory\4
+
+#directory with lots of stuff
+mkdir $directory\5
+for i in {1..100}
+	do
+		mkdir $directory\5/$i
+		touch $directory\5/$i/new$i
+done
+
+#directory set to imutible
+mkdir $directory\6
+chattr +i $directory\6
 
