a5cdfb299254cf97e791ff8653b7d74533d7c706
root <dexmount@hotmail.com>
2017-10-06 13:48:16 -0500

    added an install flag and and a remove flag

--- gauntlet.sh
+++ gauntlet.sh
@@ -2,31 +2,43 @@
 
 directory="test"
 
-#empty directory
-mkdir $directory\1
+if [ "$1" == "-i" ]; then
+	#empty directory
+	mkdir $directory\1
 
-#hidden directory
-mkdir .$directory\2
+	#hidden directory
+	mkdir .$directory\2
 
-#Non-empty directory
-mkdir $directory\3
-touch $directory\3/new
+	#Non-empty directory
+	mkdir $directory\3
+	touch $directory\3/new
 
-#directory in /etc/
-mkdir /etc/$directory\4
+	#directory in /etc/
+	mkdir /etc/$directory\4
 
-#directory with lots of stuff
-mkdir $directory\5
-for i in {1..100}
-	do
-		mkdir $directory\5/$i
-		touch $directory\5/$i/new$i
-done
+	#directory with lots of stuff
+	mkdir $directory\5
+	for i in {1..100}
+		do
+			mkdir $directory\5/$i
+			touch $directory\5/$i/new$i
+	done
 
-#directory set to imutible
-mkdir $directory\6
-chattr +i $directory\6
+	#directory set to imutible
+	mkdir $directory\6
+	chattr +i $directory\6
 
-#selinux folder permissions
-mkdir $directory\7
+	#selinux folder permissions
+	mkdir $directory\7
+fi
 
+if [ "$1" == "-r" ]; then
+	rmdir $directory\1
+	rmdir .$directory\2
+	rm -rf $directory\3
+	rmdir /etc/$directory\4
+	rm -rf $directory\5
+	chattr -i $directory\6
+	rmdir $directory\6
+	rmdir $directory\7
+fi
